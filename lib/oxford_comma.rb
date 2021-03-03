116d51bfab6ff1d8883a93eb46346e414f5e69a4
Rianna Castro <riannaxcastro@gmail.com>
2021-03-02 19:04:59 -1000

    commit tree ee3cbb67892037ca06b1518058184c077769b25d

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,30 @@
 def oxford_comma(array)
+    if array.count == 1
+        return array.join
+    elsif array.count == 2
+        return array.join(" and ")
+    else
+        array[-1] = "and " + array[-1]
+        return array.join(", ")
+    end
+
 
 end
+
+# def oxford_comma(array_new_too)
+    # array_new_too.join(" and ") do ["kiwi", "durian"]
+    # end
+
+# end
+
+# def oxford_comma(array)
+    
+    # array.join(" and ") if array.size < 3
+    #  array[-1] = "and " + array[-1]
+    # array.join(", ")
+    
+
+# end
+
+
+    
